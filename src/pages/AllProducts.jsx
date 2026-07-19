8039b2beb1b68e65d16319445a5a0c6cf1e4c75d
raihanshaikhh <raihanshaikh91201@gmail.com>
2026-07-19 18:37:40 +0530

    fixed minor bug

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,5 +1,5 @@
-import { products } from "../data/products";
-import ProductCard from "../components/ProductCard";
+import { products } from "../data/products.js";
+import ProductCard from "../components/ProductCard.jsx";
 
 export default function Products() {
     return (
