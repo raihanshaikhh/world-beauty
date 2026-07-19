c7f097b4c40e1461a776b8c34ea9aa671af9473e
raihanshaikhh <raihanshaikh91201@gmail.com>
2026-07-19 18:51:30 +0530

    edited import in product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { products } from "../data/Products.js"
+import { products } from "../data/products.js"
 import { FaWhatsapp } from "react-icons/fa";
 import { useState } from "react";
 
